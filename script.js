8b70d00a5a3788c2564e9f25907308a894dffbc0
Franco Enrici <francoenrici@MacBook-Air-de-Franco.local>
2025-03-26 16:47:47 +0100

    graficos listos

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ var data = {
   // As options we currently only set a static size of 300x200 px
   var options = {
     width: '600px',
-    height: '500px'
+    height: '500px',
+    axisY: {
+      onlyInteger: true
+  }
   };
   
   // In the global name space Chartist we call the Line function to initialize a line chart. As a first parameter we pass in a selector where we would like to get our chart created. Second parameter is the actual data object and as a third parameter we pass in our options
@@ -36,24 +39,6 @@ getmoviesInfo().then(movies => {
 
     new Chartist.Line('.ct-chart', updatedData, options);
 });
-
-var data = {
-    // A labels array that can contain any sort of values
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-    // Our series array that contains series objects or in this case series data arrays
-    series: [
-      [5, 2, 4, 2, 0]
-    ]
-  };
-  
-  // As options we currently only set a static size of 300x200 px
-  var options = {
-    width: '600px',
-    height: '500px'
-  };
-  
-  // In the global name space Chartist we call the Line function to initialize a line chart. As a first parameter we pass in a selector where we would like to get our chart created. Second parameter is the actual data object and as a third parameter we pass in our options
-  new Chartist.Line('.cT-chart', data, options);
   
 async function getCharactersInfo() {
     let response = await fetch('https://swapi.dev/api/people/');
